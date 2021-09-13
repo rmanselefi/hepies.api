317ebacbdaa815ee5702c44622627187e3603406
Razinmessele <rmessele@excellerentsolutions.com>
2021-09-13 10:48:58 +0300

    fixed comment data structure

--- src/patient/service/patient.service.ts
+++ src/patient/service/patient.service.ts
@@ -60,7 +60,7 @@ export class PatientService {
     const pres = await getConnection()
       .getRepository(PrescriptionEntity)
       .createQueryBuilder('prescription')
-      .select('prescription.code,prescription.id')
+      
       .where('prescription.patientId = :id', { id: patient_id })
       .groupBy('prescription.code')
       .execute();
