da596b190ff7db5ac8e832bee6688b16617cd0f1
Razinmessele <rmessele@excellerentsolutions.com>
2021-10-22 10:09:32 +0300

    module add

--- src/prescription/prescription.module.ts
+++ src/prescription/prescription.module.ts
@@ -7,15 +7,10 @@ import { HxEntity } from './hx.entity';
 import { PxEntity } from './px.entity';
 import { PatientEntity } from '../patient/patient.entity';
 import { InvestigationEntity } from './entities/investigation.entity';
-import { HemathologyEntity } from './entities/hemathology.entity';
-import { ChemistryEntity } from './entities/chemistry.entity';
-import { EndocrinologyEntity } from './entities/endocrinology.entity';
-import { UrineEntity } from './entities/urine.entity';
-import { SerologyEntity } from './entities/serology.entity';
-import { DrugsService } from '../drugs/services/drugs.service';
 import { DrugEntity } from '../drugs/drugs.entity';
 import { DrugsModule } from '../drugs/drugs.module';
 import { DxEntity } from './entities/dx.entity';
+import { ProffesionalEntity } from '../users/professional.entity';
 
 @Module({
   imports: [
@@ -27,6 +22,7 @@ import { DxEntity } from './entities/dx.entity';
       InvestigationEntity,
       DrugEntity,
       DxEntity,
+      ProffesionalEntity,
     ]),
     DrugsModule,
   ],
